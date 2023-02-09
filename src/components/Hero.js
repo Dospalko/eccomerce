1a5dea9c00d06c028e5a70cf227050f4e8e6a7ca
Dominik Palo <dominik.palo@student.tuke.sk>
2023-02-09 14:58:12 +0100

    added alot of new features

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,27 @@
-import React from 'react';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import WomanImg from '../img/woman_hero.png'
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="bg-pink-200 h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24">
+      <div className="container mx-auto flex justify-around h-full">
+        <div className="flex flex-col justify-center">
+          <div className="font-semibold flex items-center uppercase">
+            <div className="w-10 h-[2px] bg-gray-500 mr-3"></div>Nove trendy!
+          </div>
+          <h1 className="text-[70px] leading-[1.1] font-light mb-4">Jesenne zlavy prave teraz! <br/>
+              <span className="font-semibold">ZENY</span>
+          </h1>
+          <Link to={'/'} className="w-40 self-auto uppercase font-semibold border-b-2 border-primary">
+            Objav viac!
+          </Link>
+        </div>
+        <div className="hidden lg:block">
+          <img src={WomanImg} alt=''/>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- src/components/Product.js
+++ src/components/Product.js
@@ -2,7 +2,12 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import { BsPlus, BsEyeFill } from "react-icons/bs";
+
+import { CartContext } from "../contexts/CartContext";
 const Product = ({ product }) => {
+
+  const {addToCart} = useContext(CartContext)
+
   //destrukt
 
   const { id, image, category, title, price } = product;
@@ -19,7 +24,7 @@ const Product = ({ product }) => {
           </div>
         </div>
         <div className="absolute sm:top-2 top-6 -right-11 group-hover:right-5 bg-slate-600 p-2 flex flex-col item-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
-          <button>
+          <button onClick={ () => addToCart(product,id)}>
             <div className="flex justify-center items-center text-white w-8 h-8">
               <BsPlus className="text-2xl" />
             </div>
